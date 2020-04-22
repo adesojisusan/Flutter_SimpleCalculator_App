5a43de10b22d55232069a299e3eec165a42235c4
Adesoji Susan <42998872+adesojisusan@users.noreply.github.com>
2020-04-22 18:45:29 +0100

    Add files via upload

---
+++ lib/app_screen/home.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+
+      child: Text(
+          "flight",
+          textDirection: TextDirection.ltr
+      style: TextStyle( ),
+       ),
+
+    );
+  }
+
+}
